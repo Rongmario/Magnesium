b89df9ee6aea1332e74bd6165ad7dcd1bcdb7969
singlerr <singlerr@naver.com>
2023-10-18 09:18:11 +0900

    compatible model for 1.12.2

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MixinMultipartBakedModel.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MixinMultipartBakedModel.java
@@ -1,10 +1,16 @@
 package me.jellysquid.mods.sodium.mixin.features.model;
 
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import me.jellysquid.mods.sodium.client.util.rand.XoRoShiRoRandom;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.render.model.MultipartBakedModel;
+import net.minecraft.client.renderer.block.model.*;
+import net.minecraft.client.renderer.block.model.multipart.Multipart;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.Direction;
 import net.minecraftforge.client.model.data.IModelData;
 import org.apache.commons.lang3.tuple.Pair;
@@ -13,30 +19,33 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import javax.annotation.Nullable;
 import java.util.*;
 import java.util.concurrent.locks.StampedLock;
 import java.util.function.Predicate;
 
 @Mixin(MultipartBakedModel.class)
 public class MixinMultipartBakedModel {
-    private final Map<BlockState, BakedModel[]> stateCacheFast = new Reference2ReferenceOpenHashMap<>();
+    private final Map<IBlockState, IBakedModel[]> stateCacheFast = new Reference2ReferenceOpenHashMap<>();
     private final StampedLock lock = new StampedLock();
+
+    private final XoRoShiRoRandom random = new XoRoShiRoRandom();
 
     @Shadow
     @Final
-    private List<Pair<Predicate<BlockState>, BakedModel>> components;
+    private Map<com.google.common.base.Predicate<IBlockState>, IBakedModel> selectors;
 
     /**
      * @author JellySquid
      * @reason Avoid expensive allocations and replace bitfield indirection
      */
     @Overwrite
-    public List<BakedQuad> getQuads(BlockState state, Direction face, Random random, IModelData modelData) {
+    public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand) {
         if (state == null) {
             return Collections.emptyList();
         }
 
-        BakedModel[] models;
+        IBakedModel[] models;
 
         long readStamp = this.lock.readLock();
         try {
@@ -48,15 +57,15 @@ public class MixinMultipartBakedModel {
         if (models == null) {
             long writeStamp = this.lock.writeLock();
             try {
-                List<BakedModel> modelList = new ArrayList<>(this.components.size());
+                List<IBakedModel> modelList = new ArrayList<>(this.selectors.size());
 
-                for (Pair<Predicate<BlockState>, BakedModel> pair : this.components) {
-                    if (pair.getLeft().test(state)) {
-                        modelList.add(pair.getRight());
+                for (Map.Entry<com.google.common.base.Predicate<IBlockState>, IBakedModel> pair : this.selectors.entrySet()) {
+                    if (pair.getKey().test(state)) {
+                        modelList.add(pair.getValue());
                     }
                 }
 
-                models = modelList.toArray(new BakedModel[modelList.size()]);
+                models = modelList.toArray(new IBakedModel[modelList.size()]);
                 this.stateCacheFast.put(state, models);
             } finally {
                 this.lock.unlockWrite(writeStamp);
@@ -66,9 +75,9 @@ public class MixinMultipartBakedModel {
         List<BakedQuad> quads = new ArrayList<>();
         long seed = random.nextLong();
 
-        for (BakedModel model : models) {
+        for (IBakedModel model : models) {
             random.setSeed(seed);
-            quads.addAll(model.getQuads(state, face, random, modelData));
+            quads.addAll(model.getQuads(state, side, random.nextLong()));
         }
 
         return quads;
--- src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MixinWeightedBakedModel.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MixinWeightedBakedModel.java
@@ -1,31 +1,39 @@
 package me.jellysquid.mods.sodium.mixin.features.model;
 
+import me.jellysquid.mods.sodium.client.util.rand.XoRoShiRoRandom;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.render.model.WeightedBakedModel;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.WeightedBakedModel;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.Direction;
 import net.minecraftforge.client.model.data.IModelData;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 @Mixin(WeightedBakedModel.class)
 public class MixinWeightedBakedModel {
+
+    private XoRoShiRoRandom random = new XoRoShiRoRandom();
+
     @Shadow
     @Final
-    private List<WeightedBakedModel.Entry> models;
+    private List<WeightedBakedModel.WeightedModel> models;
 
     @Shadow
     @Final
     private int totalWeight;
 
-    private static <T extends WeightedBakedModel.Entry> T getAt(List<T> pool, int totalWeight) {
+    private static <T extends WeightedBakedModel.WeightedModel> T getAt(List<T> pool, int totalWeight) {
         int i = 0;
         int len = pool.size();
 
@@ -37,7 +45,7 @@ public class MixinWeightedBakedModel {
             }
 
             weighted = pool.get(i++);
-            totalWeight -= weighted.weight;
+            totalWeight -= weighted.itemWeight;
         } while (totalWeight >= 0);
 
         return weighted;
@@ -48,11 +56,11 @@ public class MixinWeightedBakedModel {
      * @reason Avoid excessive object allocations
      */
     @Overwrite
-    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction face, Random random, IModelData modelData) {
-        WeightedBakedModel.Entry entry = getAt(this.models, Math.abs((int) random.nextLong()) % this.totalWeight);
+    public List<BakedQuad> getQuads(@javax.annotation.Nullable IBlockState state, @Nullable EnumFacing side, long rand) {
+        WeightedBakedModel.WeightedModel entry = getAt(this.models, Math.abs((int) random.nextLong()) % this.totalWeight);
 
         if (entry != null) {
-            return entry.model.getQuads(state, face, random, modelData);
+            return entry.model.getQuads(state, side, rand);
         }
 
         return Collections.emptyList();
